1d76ad9ceb2a5121a20ea2d8c836e536f10de019
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-08 22:43:16 +0100

    Cleanup

--- src/primes-hash.js
+++ src/primes-hash.js
@@ -2,23 +2,15 @@ import { Primes } from './primes.js';
 import { pow } from './numbers.js';
 import { sha256 } from './hash.js';
 import { Secp256k1 } from './secp256k1.js';
-import { bigIntToBuffer, bufferToBigInt, randomBytes } from './buffer.js';
-
 
 export async function hashToPrime(buffer) {
-    // const hashBuffer = await sha256(buffer)
-    const privateKey = bufferToBigInt(randomBytes(32));
-    const hash = Secp256k1.G.multiply(privateKey).compress();
 
-    // const hash = bufferToBigInt(hashBuffer);
-    return Primes.factorize(hash)
+	// TODO implement me
+
+    // // const hashBuffer = await sha256(buffer)
+    // const privateKey = bufferToBigInt(randomBytes(32));
+    // const hash = Secp256k1.G.multiply(privateKey).compress();
+
+    // // const hash = bufferToBigInt(hashBuffer);
+    // return Primes.factorize(hash)
 }
-
-
-
-((async _ => {
-	const l = await hashToPrime(randomBytes(32));
-	debugger;
-})())
-
-
