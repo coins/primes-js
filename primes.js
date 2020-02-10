338ba055025431817abea2d07ece2ccb88e59631
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-10 21:03:25 +0100

    Add version

--- primes.js
+++ primes.js
@@ -1,1 +1,5 @@
+/**
+*	@module 	primes.js
+*	@version	0.0.1
+*/
 export * from './src/primes/primes.js'
--- src/primes.js
+++ src/primes.js
@@ -1,1 +1,5 @@
+/**
+*	@module 	primes.js
+*	@version	0.0.1
+*/
 import * from './primes/primes.js'
