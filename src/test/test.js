8b53000cea7f983dadcdd5103f4384a4a457d5f4
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-10 17:35:19 +0100

    Refactor test structure

---
+++ src/test/test.js
@@ -0,0 +1,4 @@
+import '../primes/test.js'
+import '../prime-counting/test.js'
+import '../primality-test/test.js'
+import '../nth-prime/test.js'
--- src/~test/~test.js
+++
@@ -1,5 +0,0 @@
-import './jasmine-3.5.0/boot.js'
-import '../primes/~test.js'
-import '../prime-counting/~test.js'
-import '../primality-test/~test.js'
-import '../nth-prime/~test.js'
