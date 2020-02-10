f16b5a24a59040429ee84cbad1842cf081f7f54c
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-10 13:03:23 +0100

    Cleanup

--- src/nth-prime/nth-prime.js
+++ src/nth-prime/nth-prime.js
@@ -9,8 +9,8 @@ import { countPrimes } from '../prime-counting/prime-counting.js'
  *
  */
 export function nthPrime(n) {
-    const min = n * log10(n) 
-    const max = n * log2(n) 
+    const min = n * log10(n) // rough lower limit
+    const max = n * ( log2(n) + 100 ) // very rough upper limit
     let searchResult = binarySearch(n, min, max)
     while (countPrimes(searchResult) === countPrimes(searchResult - 1)) 
     	searchResult--; // TODO: Do binary search here
@@ -36,7 +36,7 @@ function binarySearch(n, min, max) {
 
 
 function log2(n) {
-    const bits = Math.round(n.toString(16).length / 2) + 100
+    const bits = Math.round(n.toString(16).length / 2)
     return bits
 }
 
@@ -46,3 +46,5 @@ function log10(n) {
 }
 
 // TODO: adapt to BigInt
+// TODO: check for Number.MAX_SAFE_INTEGER
+// TODO: make it work for bigger numbers. Binary search searchResult?
